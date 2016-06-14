f33903228b03a890c607070327583640526ed978
eric.laird@gmail.com <eric.laird@gmail.com>
2016-06-14 10:46:19 +0800

    Change to use SWIG template engine

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+swig=require('swig');
+var swig=new swig.Swig();
+app.engine('html',swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
